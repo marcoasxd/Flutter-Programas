0cdf19c64d9077510f069e6005a497910882a0e3
Marco Antonio Mendes <102104507+marcoasxd@users.noreply.github.com>
2023-10-07 19:20:00 -0300

    Updated

--- lib/index.dart
+++ lib/index.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, annotate_overrides, use_key_in_widget_constructors, unnecessary_new
+// ignore_for_file: prefer_const_constructors, annotate_overrides, use_key_in_widget_constructors, unnecessary_new, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -9,6 +9,13 @@ void main(List<String> args) {
 class Cod1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final perguntas = [
+      'Qual a sua comida favorita?',
+      'Qual o seu animal favorito',
+      'Qual a sua cor favorita?',
+    ];
+
     return MaterialApp(
       // Invés de receber um texto, estou retornando o Scaffold para mais opções de personalização.
       home: Scaffold(
@@ -18,7 +25,23 @@ class Cod1 extends StatelessWidget {
           backgroundColor: Colors.red,
           centerTitle: true,
         ),
-        body: Text('Olá Flutter.'),
+        body: Center(
+          child: Column(
+            children: [
+              Text('Qual é a pergunta 1?'),
+              Text(perguntas[0]),
+              Text('Qual é a pergunta 2?'),
+              Text(perguntas[1]),
+              Text('Qual é a pergunta 3?'),
+              Text(perguntas[2]),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.red), 
+                child: Text('Responder pergunta'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
